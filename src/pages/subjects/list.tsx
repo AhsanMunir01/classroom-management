1cc3d8cb2f84844bcc5ad24512c4b1313749e1ce
Ahsan Munir <f228787@cfd.nu.edu.pk>
2026-07-18 00:23:33 +0500

    implement the list of subject

--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -1,8 +1,39 @@
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb";
+import { ListView } from "@/components/refine-ui/views/list-view.tsx"
+import { Search } from "lucide-react";
+import { useState } from "react";
+
+
+import { Input } from "@/components/ui/input";
+
 const SubjectsList = () => {
+    const [searchQuery, setSearchQuery] = useState('');
+
+
     return (
-        <div>
-            List
-        </div>
+        <ListView>
+            <Breadcrumb />
+
+            <h1 className="page-title">Subjects</h1>
+
+            <div className="intro-row">
+                <p>Quick access to essential metrics and management tools</p>
+
+                <div className="action-row">
+                    <div className="search-field">
+                        <Search className="search-icon" />
+                        <Input
+                            type="text"
+                            placeholder="Search subjects..."
+                            className="pl-10 w-full"
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
+                        />
+
+                    </div>
+                </div>
+            </div>
+        </ListView>
     )
 }
 
